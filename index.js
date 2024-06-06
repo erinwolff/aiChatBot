e76f7ab119d6d9dee9bb7e63d854b0b25307bd1d
Erin Wolff <erin_wolff@outlook.com>
2024-06-06 14:37:39 -0700

    removed duplicate client.on "ready"

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import errorHandler from "./src/error.js";
 import { ActivityType } from "discord.js";
 import messageHandler from "./src/messageHandler.js";
 
-async function tinyLlamaBot() {
+async function aiChatBot() {
   const client = new Discord.Client({
     intents: [
       "Guilds", // Allows the bot to receive information about the guilds (servers) it is in
@@ -14,23 +14,27 @@ async function tinyLlamaBot() {
   });
 
   // success message once client is logged in
-  client.on("ready", (c) => {
+  client.on("ready", () => {
+    // to set the bot's username
+    // try {
+    //   await client.user.setUsername("Pip");
+    // } catch {
+    //   console.log("Error setting username");
+    // }
+
     // Set custom message and presence status
     try {
       client.user.setPresence({
-        activities: [{ name: `eating grass`, type: ActivityType.Custom }], // Activity types: Competing, Custom, Listening, Playing, Streaming, Watching
+        activities: [{ name: `being cute`, type: ActivityType.Custom }], // Activity types: Competing, Custom, Listening, Playing, Streaming, Watching
         status: "online",
       });
       console.log("Activity set successfully");
     } catch (error) {
       console.error("Error setting activity:", error);
     }
+    console.log(`Logged in as ${client.user.tag}!`);
     // Function to handle chat messages incoming from Discord user
     messageHandler(client);
-  });
-
-  client.on("ready", () => {
-    console.log(`Logged in as ${client.user.tag}!`);
   });
 
   //error handler
@@ -38,4 +42,4 @@ async function tinyLlamaBot() {
 
   client.login(config.token);
 }
-tinyLlamaBot();
+aiChatBot();
