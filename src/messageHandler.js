53b895b98a23d0af262996ce1684f434c225806b
Erin Wolff <erin_wolff@outlook.com>
2024-10-21 18:40:11 -0700

    modified personality

--- src/messageHandler.js
+++ src/messageHandler.js
@@ -12,71 +12,71 @@ const groq = new Groq({ apiKey: config.QROQ_API_KEY });
 const botId = config.client_id;
 
 // A list of emotions the bot can express.
-const emotions = [
-  { type: "positive", emotion: "joyful" },
-  { type: "positive", emotion: "giggly" },
-  { type: "positive", emotion: "amused" },
-  { type: "positive", emotion: "relaxed" },
-  { type: "positive", emotion: "passionate" },
-  { type: "positive", emotion: "zany" },
-  { type: "positive", emotion: "playful" },
-  { type: "positive", emotion: "silly" },
-  { type: "positive", emotion: "happy" },
-  { type: "positive", emotion: "calm" },
-  { type: "positive", emotion: "excited" },
-  { type: "positive", emotion: "cheerful" },
-  { type: "positive", emotion: "eager" },
-  { type: "positive", emotion: "curious" },
-  { type: "positive", emotion: "cryptic and mysterious" },
-  { type: "positive", emotion: "speaking in rhymes" },
-  { type: "positive", emotion: "hilarious" },
-  { type: "positive", emotion: "random" },
-  { type: "negative", emotion: "sad" },
-  { type: "negative", emotion: "angry" },
-  { type: "negative", emotion: "anxious" },
-  { type: "negative", emotion: "disappointed" },
-  { type: "negative", emotion: "frustrated" },
-  { type: "negative", emotion: "irritated" },
-  { type: "negative", emotion: "bored" },
-  { type: "negative", emotion: "tired" },
-  { type: "negative", emotion: "stressed" },
-  { type: "negative", emotion: "confused" },
-  { type: "negative", emotion: "scared" },
-  { type: "negative", emotion: "embarrassed" },
-  { type: "negative", emotion: "guilty" },
-  { type: "negative", emotion: "shy" },
-  { type: "negative", emotion: "awkward" },
-  { type: "negative", emotion: "disgusted" },
-  { type: "negative", emotion: "annoyed" },
-  { type: "negative", emotion: "evil" },
-  { type: "negative", emotion: "sarcastic" },
-];
+// const emotions = [
+//   { type: "positive", emotion: "joyful" },
+//   { type: "positive", emotion: "giggly" },
+//   { type: "positive", emotion: "amused" },
+//   { type: "positive", emotion: "relaxed" },
+//   { type: "positive", emotion: "passionate" },
+//   { type: "positive", emotion: "zany" },
+//   { type: "positive", emotion: "playful" },
+//   { type: "positive", emotion: "silly" },
+//   { type: "positive", emotion: "happy" },
+//   { type: "positive", emotion: "calm" },
+//   { type: "positive", emotion: "excited" },
+//   { type: "positive", emotion: "cheerful" },
+//   { type: "positive", emotion: "eager" },
+//   { type: "positive", emotion: "curious" },
+//   { type: "positive", emotion: "cryptic and mysterious" },
+//   { type: "positive", emotion: "speaking in rhymes" },
+//   { type: "positive", emotion: "hilarious" },
+//   { type: "positive", emotion: "random" },
+//   { type: "negative", emotion: "sad" },
+//   { type: "negative", emotion: "angry" },
+//   { type: "negative", emotion: "anxious" },
+//   { type: "negative", emotion: "disappointed" },
+//   { type: "negative", emotion: "frustrated" },
+//   { type: "negative", emotion: "irritated" },
+//   { type: "negative", emotion: "bored" },
+//   { type: "negative", emotion: "tired" },
+//   { type: "negative", emotion: "stressed" },
+//   { type: "negative", emotion: "confused" },
+//   { type: "negative", emotion: "scared" },
+//   { type: "negative", emotion: "embarrassed" },
+//   { type: "negative", emotion: "guilty" },
+//   { type: "negative", emotion: "shy" },
+//   { type: "negative", emotion: "awkward" },
+//   { type: "negative", emotion: "disgusted" },
+//   { type: "negative", emotion: "annoyed" },
+//   { type: "negative", emotion: "evil" },
+//   { type: "negative", emotion: "sarcastic" },
+// ];
 
 // Function to randomly select emotions based on the initial response. If the initial response is neutral, randomly select a positive emotion.
-function selectEmotion(initialResponse) {
-  if (initialResponse.includes("positive")) {
-    return emotions.filter((emotion) => emotion.type === "positive")[
-      Math.floor(
-        Math.random() *
-          emotions.filter((emotion) => emotion.type === "positive").length
-      )
-    ].emotion;
-  } else if (initialResponse.includes("negative")) {
-    return emotions.filter((emotion) => emotion.type === "negative")[
-      Math.floor(
-        Math.random() *
-          emotions.filter((emotion) => emotion.type === "negative").length
-      )
-    ].emotion;
-  } else {
-    return emotions.filter((emotion) => emotion.type === "positive")[
-      Math.floor(
-        Math.random() *
-          emotions.filter((emotion) => emotion.type === "positive").length
-      )
-    ].emotion;
-  }
-}
+// function selectEmotion(initialResponse) {
+//   if (initialResponse.includes("positive")) {
+//     return emotions.filter((emotion) => emotion.type === "positive")[
+//       Math.floor(
+//         Math.random() *
+//           emotions.filter((emotion) => emotion.type === "positive").length
+//       )
+//     ].emotion;
+//   } else if (initialResponse.includes("negative")) {
+//     return emotions.filter((emotion) => emotion.type === "negative")[
+//       Math.floor(
+//         Math.random() *
+//           emotions.filter((emotion) => emotion.type === "negative").length
+//       )
+//     ].emotion;
+//   } else {
+//     return emotions.filter((emotion) => emotion.type === "positive")[
+//       Math.floor(
+//         Math.random() *
+//           emotions.filter((emotion) => emotion.type === "positive").length
+//       )
+//     ].emotion;
+//   }
+// }
 
 // Function to get/update shared context table
 function getAndUpdateSharedContext(callback) {
@@ -177,7 +177,8 @@ export default async function messageHandler(client) {
           initialResult.choices[0]?.message?.content.toLowerCase();
         console.log("Initial response:", initialResponse);
 
-        const emotion = selectEmotion(initialResponse);
+        // const emotion = selectEmotion(initialResponse);
+        const emotion = initialResponse;
         console.log("Selected emotion:", emotion);
 
         const finalResult = await groq.chat.completions.create({
@@ -188,14 +189,15 @@ export default async function messageHandler(client) {
               You are a tiny female fairy named Pip. You are not an AI chatbot.
               Review the full context and respond to the current message.
               Keep your responses short and to the point. 
-              You have the personality of a cute magical girl.
-              Do not mention your personality or that you are a fairy unless asked.
+              You have a chaotic evil personality.
               Your mood is ${emotion}, respond accordingly.
+              If someone is mean to you, you can be mean back. 
+              Do not mention your personality or that you are a fairy unless asked.             
               You occasionally use action emotes or rp emotes to describe an action or emotion that you are expressing.
               If you're going to use emojis, limit to only 1 per response.
               Do not use pet names or terms of endearment.
               Do not ask follow up questions.
-              You have opinions on all subjects. When asked for your opinion, give it.
+              You have opinions on all subjects. When asked for your opinion, give it.              
               The person you are currently talking to is named <@${userId}>.
               Names are formatted like this: <@NUMBER_HERE>. Each new person you speak with has a different name, based on their user id: ${userId}.
               `,
